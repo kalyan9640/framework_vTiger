450fbe470ca1ec4fc574747c59b73bd0ab78f9dc
divya <divya@PavanKalyan.mshome.net>
2024-06-08 13:20:45 +0530

    added new pom class  product page is added

---
+++ vTiger_CRm/src/main/java/com/vTiger/crm/ObjectRepositoryUtility/ProductsPage.java
@@ -0,0 +1,22 @@
+package com.vTiger.crm.ObjectRepositoryUtility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductsPage {
+	WebDriver driver = null;
+
+	public ProductsPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//img[@alt='Create Product...']")
+	private WebElement createProduct;
+
+	public WebElement getCreateProduct() {
+		return createProduct;
+	}
+}
